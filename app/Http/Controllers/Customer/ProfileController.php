351d4297c7bb4fb9f93f0409e065a00f963ba9d5
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 20:55:02 +0700

    Develop - добавлен контроллер Controllers/Customer/ProfileController.php

---
+++ app/Http/Controllers/Customer/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\JsonResponse;
+
+class ProfileController extends Controller
+{
+    public function show(): UserResource
+    {
+        return new UserResource(auth()->user());
+    }
+}
