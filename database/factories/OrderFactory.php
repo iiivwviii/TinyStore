d833ec4de047a4477dc26086f15063c45d38f307
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 21:17:38 +0700

    Develop - добавлена фабрика database/factories/OrderFactory.php

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'user_id' => \App\Models\User::factory(),
+            'total' => fake()->randomFloat(2, 10, 1000),
+            'status' => fake()->randomElement(Order::STATUSES),
+        ];
+    }
+}
