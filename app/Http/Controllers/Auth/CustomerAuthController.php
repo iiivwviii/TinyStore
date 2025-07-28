cfa95189250ff25fd6b31929f335edbeabf40720
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 20:54:16 +0700

    Develop - добавлен контроллер Controllers/Auth/CustomerAuthController.php

---
+++ app/Http/Controllers/Auth/CustomerAuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class CustomerAuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'is_admin' => false,
+        ]);
+
+        return response()->json([
+            'token' => $user->createToken('customer_token')->plainTextToken,
+        ]);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        return response()->json([
+            'token' => $request->user()->createToken('customer_token')->plainTextToken,
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out']);
+    }
+}
