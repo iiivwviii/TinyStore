364036fe20a85ed0295991f2f365d5557c898ace
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 20:57:16 +0700

    Develop - добавлен контроллер Controllers/Admin/UserController.php

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class UserController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        $users = User::where('is_admin', false)->paginate();
+
+        return UserResource::collection($users);
+    }
+}
