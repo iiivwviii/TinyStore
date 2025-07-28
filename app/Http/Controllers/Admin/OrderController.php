3c8496686d96a9c7e7197086cdc65f3fcba05605
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 20:57:44 +0700

    Develop - добавлен контроллер Controllers/Admin/OrderController.php

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\UpdateOrderRequest;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use App\Services\OrderService;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class OrderController extends Controller
+{
+    public function __construct(private OrderService $orderService)
+    {
+    }
+
+    public function index(): AnonymousResourceCollection
+    {
+        $orders = Order::with(['user', 'items.product'])->paginate();
+
+        return OrderResource::collection($orders);
+    }
+
+    public function update(UpdateOrderRequest $request, Order $order): OrderResource
+    {
+        $this->orderService->updateStatus($order, $request->status);
+
+        return new OrderResource($order->load(['user', 'items.product']));
+    }
+}
