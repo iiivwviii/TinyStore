d40c618626136e4b75983c13439bdb5c84d2d25c
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 21:02:46 +0700

    Develop - добавлены REQUEST Http/Requests/CreateOrderRequest.php

---
+++ app/Http/Requests/CreateOrderRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Customer;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateOrderRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'items' => ['required', 'array', 'min:1'],
+            'items.*.product_id' => ['required', 'exists:products,id'],
+            'items.*.quantity' => ['required', 'integer', 'min:1'],
+        ];
+    }
+}
