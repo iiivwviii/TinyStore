18d0f1e093ec386a94659ef862691e03b0b6f43a
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 20:52:24 +0700

    Develop - добавлен контроллер Controllers/CategoryController.php

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+
+class CategoryController extends Controller
+{
+    public function index(): AnonymousResourceCollection
+    {
+        $categories = Category::withCount('products')->paginate();
+
+        return CategoryResource::collection($categories);
+    }
+}
