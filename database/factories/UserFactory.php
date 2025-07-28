e010dae0afd7bc6c35aec183c488195b40cd3ca9
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-07-28 21:16:27 +0700

    Develop - добавлена фабрика database/factories/UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,21 +3,28 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'balance' => fake()->randomFloat(2, 100, 10000),
+            'is_admin' => false,
+            'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'is_admin' => true,
+        ]);
     }
 }
